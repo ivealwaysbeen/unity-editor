6c5df23c791f1c64f99ed18f3b2187b8a583ca9a
정문경 <developer.frontdoor@gmail.com>
2022-01-04 23:28:27 +0900

    Custom EditorWindow

---
+++ unity-editor/Assets/Editor/Scripts/Common/EditorWindow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/*
+ * Customized EditorWindow
+ * 
+ */
+
+public abstract class AEditorWindow : UnityEditor.EditorWindow
+{
+    protected abstract void OnGUI();
+}
+public class EditorWindow : AEditorWindow
+{
+    protected override sealed void OnGUI()
+    {
+        BaseOnGUI();
+        NextOnGUI();
+    }
+
+    //called in the OnGUI function of all classes that inherit EditorWindow
+    void BaseOnGUI()
+    {
+        
+    }
+    
+    //class that inherit EditorWindow use this method instead of OnGUI()
+    //if you want to make EditorWindow to abstract class, change this method to abstract...
+    protected virtual void NextOnGUI(){}
+
+
+
+}
