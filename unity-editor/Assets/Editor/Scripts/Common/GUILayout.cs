e812cce7f803419cc969bf8a616cb541e795c847
정문경 <developer.frontdoor@gmail.com>
2022-01-15 20:08:28 +0900

    GUILayout 툴팁 캐싱

--- unity-editor/Assets/Editor/Scripts/Common/GUILayout.cs
+++ unity-editor/Assets/Editor/Scripts/Common/GUILayout.cs
@@ -12,106 +12,158 @@ using UnityEngine;
 /// 
 public class GUILayout : UnityEngine.GUILayout
 {
-    
+    public static Dictionary<string, GUIContent> contentList = new Dictionary<string, GUIContent>();
+
+    static void AddContentList(GUIContent content)
+    {
+        if(contentList.ContainsKey(content.text) == false)
+        {
+            contentList.Add(content.text, content);
+        }
+    }
 
     public static new bool Button(GUIContent content, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.Button(content, options);
     }
     public static new bool Button(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.Button(content, style, options);
     }
 
     public static new void Label(GUIContent content, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.Label(content, options);
     }
     public static new void Label(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.Label(content, style, options);
     }
     public static new void Box(GUIContent content, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.Box(content, options);
     }
     public static new void Box(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.Box(content, style, options);
     }
     public static new bool RepeatButton(GUIContent content, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.RepeatButton(content, options);
     }
     public static new bool RepeatButton(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.RepeatButton(content, style, options);
     }
     public static new bool Toggle(bool value, GUIContent content, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.Toggle(value, content, options);
     }
     public static new bool Toggle(bool value, GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.Toggle(value, content, style, options);
     }
     public static new int Toolbar(int selected, GUIContent[] contents, params GUILayoutOption[] options)
     {
+        foreach(var content in contents)
+        {
+            AddContentList(content);
+        }
         return UnityEngine.GUILayout.Toolbar(selected, contents, options);
     }
     public static new int Toolbar(int selected, GUIContent[] contents, GUIStyle style, params GUILayoutOption[] options)
     {
+        foreach (var content in contents)
+        {
+            AddContentList(content);
+        }
         return UnityEngine.GUILayout.Toolbar(selected, contents, style, options);
     }
 
     public static new int Toolbar(int selected, GUIContent[] contents, GUIStyle style, GUI.ToolbarButtonSize buttonSize, params GUILayoutOption[] options)
     {
+        foreach (var content in contents)
+        {
+            AddContentList(content);
+        }
         return UnityEngine.GUILayout.Toolbar(selected, contents, style, buttonSize, options);
     }
 
     public static new int Toolbar(int selected, GUIContent[] contents, bool[] enabled, GUIStyle style, params GUILayoutOption[] options)
     {
+        foreach (var content in contents)
+        {
+            AddContentList(content);
+        }
         return UnityEngine.GUILayout.Toolbar(selected, contents, enabled, style, options);
     }
 
     public static new int Toolbar(int selected, GUIContent[] contents, bool[] enabled, GUIStyle style, GUI.ToolbarButtonSize buttonSize, params GUILayoutOption[] options)
     {
+        foreach (var content in contents)
+        {
+            AddContentList(content);
+        }
         return UnityEngine.GUILayout.Toolbar(selected, contents, enabled, style, buttonSize, options);
     }
 
     public static new int SelectionGrid(int selected, GUIContent[] content, int xCount, params GUILayoutOption[] options)
     {
+        foreach (var c in content)
+        {
+            AddContentList(c);
+        }
         return UnityEngine.GUILayout.SelectionGrid(selected, content, xCount, options);
     }
     public static new int SelectionGrid(int selected, GUIContent[] contents, int xCount, GUIStyle style, params GUILayoutOption[] options)
     {
+        foreach (var content in contents)
+        {
+            AddContentList(content);
+        }
         return UnityEngine.GUILayout.SelectionGrid(selected, contents, xCount,style, options);
     }
 
     public static new void BeginHorizontal(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.BeginHorizontal(content, style, options);
     }
     public static new void BeginVertical(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.BeginVertical(content, style, options);
     }
 
     public static new void BeginArea(Rect screenRect, GUIContent content)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.BeginArea(screenRect,content);
     }
     public static new void BeginArea(Rect screenRect, GUIContent content, GUIStyle style)
     {
+        AddContentList(content);
         UnityEngine.GUILayout.BeginArea(screenRect, content,style);
     }
 
     public static new Rect Window(int id, Rect screenRect, GUI.WindowFunction func, GUIContent content, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.Window(id,screenRect,func,content,options);
     }
     public static new Rect Window(int id, Rect screenRect, GUI.WindowFunction func, GUIContent content, GUIStyle style, params GUILayoutOption[] options)
     {
+        AddContentList(content);
         return UnityEngine.GUILayout.Window(id, screenRect, func, content, style, options);
     }
 
