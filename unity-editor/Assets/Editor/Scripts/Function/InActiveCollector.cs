48e1077cc1a6e633c1aafc1d08348c154467984d
정문경 <developer.frontdoor@gmail.com>
2022-01-22 17:47:11 +0900

    비활성화 오브젝트 콜렉터 구현

--- unity-editor/Assets/Editor/Scripts/Function/InActiveCollector.cs
+++ unity-editor/Assets/Editor/Scripts/Function/InActiveCollector.cs
@@ -5,6 +5,9 @@ using UnityEditor;
 
 public class InActiveCollector : EditorWindow
 {
+    GameObject objectPool;
+    bool onlyRootObject = true;
+
     [MenuItem("기능/비활성화 오브젝트 콜렉터",false,(int)Editor.Define.비활성화_오브젝트_모으기)]
     static void OpenWindow()
     {
@@ -15,6 +18,7 @@ public class InActiveCollector : EditorWindow
         if (GUILayout.ValidateApplicationIsPlaying() == false)
             return;
 
+        onlyRootObject = GUILayout.Toggle(onlyRootObject, new GUIContent("루트 오브젝트만", "토글이 체크되어 있을 경우, 하이라키에 존재하는 루트 오브젝트만 수집합니다."));
 
         GUILayout.ButtonClicker("가져오기", "비활성화 상태인 게임 오브젝트를 가져옵니다", GetInActiveGameObject);
 
@@ -22,6 +26,39 @@ public class InActiveCollector : EditorWindow
 
     void GetInActiveGameObject()
     {
-        Debug.Log("비활성화 오브젝트 가져옴.");
+        if(objectPool == null)
+        {
+            objectPool = new GameObject("ObjectPool");
+        }
+
+        var gameObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+
+        if(onlyRootObject == false)
+        {
+            foreach(var go in gameObjects)
+            {
+                for(int i = 0; i < go.transform.childCount; i++)
+                {
+                    var childGo = go.transform.GetChild(i).gameObject;
+                    if (childGo.activeSelf == false)
+                    {
+                        childGo.transform.SetParent(objectPool.transform);
+                    }
+                }
+            }
+        }
+
+        if (gameObjects == null)
+            return;
+
+        foreach (var go in gameObjects)
+        {
+            if (go.activeSelf == false)
+            {
+                go.transform.SetParent(objectPool.transform);
+            }
+        }
     }
+
+    
 }
